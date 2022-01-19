f76b69f40b63f731de550dfebdfaeb6fe861aac2
George <giftimie902@gmail.com>
2022-01-19 19:23:09 +0100

    gitignore

--- Config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class Config
-{
-    static public $db_host = "localhost";
-    static public $db_name = "cybersoup";
-    static public $db_username = "root";
-    static public $db_password = "";
-}
