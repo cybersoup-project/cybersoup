f7a9ba3540469bd1b2414821b80497d0f819b94f
Borja <borcaso@gmail.com>
2022-01-20 19:04:30 +0100

    config

---
+++ Config.php
@@ -0,0 +1,14 @@
+<?php
+
+class Config{
+
+    public static $db_host="localhost";
+    public static $db_name="cybersoup";
+    public static $db_username="root";
+    public static $db_password="";
+
+}
+
+
+
+?>
