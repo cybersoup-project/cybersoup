df3910cb323db1f7546a889684bfd8483feed6a0
George <giftimie902@gmail.com>
2022-01-21 17:09:32 +0100

    user model

--- model/Usuario.php
+++ model/Usuario.php
@@ -6,27 +6,34 @@ require_once("model/connection.php");
 class Usuario extends Connection
 {
     public function getUserId($u) {
-        return $this->db->query("SELECT `id` FROM `usuarios` WHERE `usuario` = '$u'", PDO::FETCH_ASSOC)->fetch()['id'];
+        return $this->db->query("SELECT `iduser` FROM `user` WHERE `username` = '$u'", PDO::FETCH_ASSOC)->fetch()['iduser'];
     }
 
     public function getUserById($id)
     {
-        return $this->db->query("SELECT * FROM `usuarios` WHERE `ID` = $id", PDO::FETCH_ASSOC)->fetch();
+        return $this->db->query("SELECT * FROM `user` WHERE `iduser` = $id", PDO::FETCH_ASSOC)->fetch();
     }
 
     public function getUserByUsername($u)
     {
-        return $this->db->query("SELECT * FROM `usuarios` WHERE `usuario` = '$u'", PDO::FETCH_ASSOC)->fetch();
+        return $this->db->query("SELECT * FROM `user` WHERE `username` = '$u'", PDO::FETCH_ASSOC)->fetch();
     }
 
     public function getUserByEmail($e)
     {
-        return $this->db->query("SELECT * FROM `usuarios` WHERE `email` = '$e'", PDO::FETCH_ASSOC)->fetch();
+        return $this->db->query("SELECT * FROM `user` WHERE `email` = '$e'", PDO::FETCH_ASSOC)->fetch();
     }
 
-    public function setUsuario($usuario, $pw, $email, $fnacimiento, $genero, $nombre)
+    public function setUsuario($usuario, $pw, $full_name, $email, $active, $role, $nombre)
     {
-        $sql = "INSERT INTO usuarios (usuario, pass, activo, email, fNacimiento, sexo, nombre) VALUES (?,?,1,?,?,?,?)";
-        $this->db->prepare($sql)->execute([$usuario, $pw, $email, $fnacimiento, $genero, $nombre]);
+        $creation_date = date("Y-m-d");
+        $sql = "INSERT INTO user (username, password, full_name, email, active, role, creation_date) VALUES (?,?,?,?,?,?,?)";
+        $this->db->prepare($sql)->execute([$usuario, $pw, $full_name, $email, $active, $role, $creation_date]);
     }
+
+    public function setValidateUserById($id) {
+        $sql = "UPDATE user SET active=1 WHERE iduser=?";
+        $this->db->prepare($sql)->execute([$id]);
+    }
+
 }
