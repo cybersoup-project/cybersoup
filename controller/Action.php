17df6fc5b0e56989b0e216fc453c917093d597d8
Alejandro <b12alejandro15@gmail.com>
2022-01-21 16:34:10 +0100

    listphp-am

--- controller/Action.php
+++ controller/Action.php
@@ -156,4 +156,7 @@ class Action {
     function profile(){
         echo $this->twig->render('profile.html');
     }
+    function listChallengers(){
+        echo $this->twig->render('ChallengesList.html');
+    }
 }
