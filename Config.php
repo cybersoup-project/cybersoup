5c3affb2131ecc00ff45dcf6bf28aef673b8915e
George <giftimie902@gmail.com>
2022-02-09 02:11:27 +0100

    mailing

--- Config.php
+++ Config.php
@@ -2,10 +2,14 @@
 
 class Config{
 
-    public static $db_host="localhost";
-    public static $db_name="cybersoup";
-    public static $db_username="root";
-    public static $db_password="";
+    public static $db_host          = "localhost";
+    public static $db_name          = "cybersoup";
+    public static $db_username      = "root";
+    public static $db_password      = "";
+
+    public static $smtp_host        = "";
+    public static $smtp_username    = "";
+    public static $smtp_password    = "";
 
 }
 
