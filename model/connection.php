b49e25c95aaca2c1ba3ab90f0bc7516ba60af61d
George <giftimie902@gmail.com>
2022-02-10 18:29:57 +0100

    connection error handling

--- model/connection.php
+++ model/connection.php
@@ -6,8 +6,12 @@ class Connection {
     public function __construct() {
         require_once("Config.php");
         $config = Config::getConfigObject();
-        $this->db = new PDO('mysql:host=' . $config->getEnvValue("DB_HOST") . ';dbname=' . $config->getEnvValue("DB_NAME") . '', $config->getEnvValue("DB_USERNAME"), $config->getEnvValue("DB_PASSWORD"));
-        $this->db->exec("set names utf8");
-        $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        try {   
+            $this->db = new PDO('mysql:host=' . $config->getEnvValue("DB_HOST") . ';dbname=' . $config->getEnvValue("DB_NAME") . '', $config->getEnvValue("DB_USERNAME"), $config->getEnvValue("DB_PASSWORD"));
+            $this->db->exec("set names utf8");
+            $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch(Exception $e) {
+            header("Location: 500.php");
+        }
     }
 }
