7b37f15693ff25d6402a610483a4309f696a04bc
Borja <borcaso@gmail.com>
2022-02-11 17:23:30 +0100

    ranking

--- model/Usuario.php
+++ model/Usuario.php
@@ -37,13 +37,21 @@ class Usuario extends Connection
     }
 
     public function getRanking() { 
-        return $this->db->query("SELECT `iduser`, `username`, `score` FROM `user` ORDER BY `score` DESC", PDO::FETCH_ASSOC)->fetchAll();
+        return $this->db->query("SELECT `iduser`, `username`, `score` FROM `user` ORDER BY `score` DESC ", PDO::FETCH_ASSOC)->fetchAll();
+    }
+    public function getRankingTopTen() { 
+        return $this->db->query("SELECT `iduser`, `username`, `score` FROM `user` ORDER BY `score` DESC LiMIT 10 ", PDO::FETCH_ASSOC)->fetchAll();
     }
     public function getMyRanking($user_id) { 
         return $this->db->query("SELECT `username`, `score` FROM `user` WHERE `iduser` = $user_id", PDO::FETCH_ASSOC)->fetchAll();
     }
-   public function role($user_id){
+    public function role($user_id){
         return $this->db->query("SELECT `role` FROM `user` WHERE `iduser`=$user_id",PDO::FETCH_ASSOC)->fetch();
    }
+   public function getUserRanking($user_id){
+     $this->db->query("SET @row_num=0;");
+    return $this->db->query(" SELECT * FROM(SELECT(@row_num:=@row_num + 1) AS num,  `iduser`, `username`, `score` FROM `user`  ORDER BY `user`.`score`  DESC) as tb WHERE tb.`iduser`=$user_id;",PDO::FETCH_ASSOC)->fetch();
+   }
 }
+
 
